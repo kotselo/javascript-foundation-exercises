a8c9662f81ecb1488c553c97474d8851fd09b0eb
Carlo Pamenting <carlo.pamenting14@gmail.com>
2023-03-04 22:12:07 +0800

    Finished simple console rock-paper-scissor game

--- script.js
+++ script.js
@@ -2,7 +2,59 @@ let name = "Carlo Pamenting";
     console.log(name);
     document.write(name);
 
-function hello(){
-    alert("Hello!");
+const hello = document.getElementById("button");
+
+function sayHello(){
+    alert("Hello! Yeah Boy!");
+} 
+
+///////////////////START////
+ const choices = ["Rock", "Paper", "Scissors"];
+ 
+ function getComputerChoice(){
+    const random = Math.floor(Math.random() * choices.length);
+    return choices[random];
+ }
+
+
+ const computerSelection = getComputerChoice();
+
+ function playRound(playerSelection, computerSelection){
+    if(playerSelection.toUpperCase() === computerSelection.toUpperCase()){
+        return "The result is a Tie!"
+    }
+    if(playerSelection.toUpperCase() === "ROCK"){
+        if(computerSelection.toUpperCase() === "PAPER"){
+            return "You Lost to the Alien! It's choice is paper."
+        } 
+        if(computerSelection.toUpperCase() === "SCISSORS"){
+            return "You Won! It's choice is scissors!"
+        }
+    }
+    if(playerSelection.toUpperCase() === "PAPER"){
+        if(computerSelection.toUpperCase() === "ROCK"){
+            return "You Won! It's Choice is Rock!!!!!!!."
+        } 
+        if(computerSelection.toUpperCase() === "SCISSORS") {
+            return "You Lost to an Alien! It's choice is scissors!"
+        }
+    }
+    if(playerSelection.toUpperCase() === "SCISSORS"){
+        if(computerSelection.toUpperCase() === "ROCK"){
+            return "You Lost to an Alien! It's choice is rock"
+        } 
+        if(computerSelection.toUpperCase() === "PAPER"){
+            return "You Won! It's choice is paper"
+        }
+    }
+ }
+
+ //console.log(playRound(playerSelection, computerSelection));
+ //const playerSelector = prompt("Choose your Bet!", "[Rock, Paper, Scissors]");
+function game(){
+    for(let i = 0; i<5;i++){
+         let playerSelection = prompt("What's your choice");
+        console.log(playRound(playerSelection, computerSelection));
+    }
 }
-document.getElementsByClassName("button").onclick =  hello;
+console.log(game());
