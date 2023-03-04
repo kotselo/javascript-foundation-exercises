548723a595e280facba238adf18b29a33ddd19e6
Carlo Pamenting <carlo.pamenting14@gmail.com>
2023-03-04 22:08:22 +0800

    Initial and foundational commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = "Carlo Pamenting";
+    console.log(name);
+    document.write(name);
+
+function hello(){
+    alert("Hello!");
+}
+document.getElementsByClassName("button").onclick =  hello;
